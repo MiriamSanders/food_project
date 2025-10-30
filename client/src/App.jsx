2604662d7ebc26c8e51151bb41eb3a96c91dc100
shiffy01 <sscheine@g.jct.ac.il>
2025-10-30 14:14:58 +0200

    styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./shiffy/Login";
 import SignupPage from "./shiffy/Signup";
-
+import FoodDonationPage from "./components/FoodDonationPage";
+import FoodItemsForm from "./components/FoodItemsForm";
 export default function App() {
   return (
     <Router>
@@ -10,6 +11,10 @@ export default function App() {
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/donation" element={<FoodDonationPage />} />
+        <Route path="/items" element={<FoodItemsForm />} />
+
+
       </Routes>
     </Router>
   );
